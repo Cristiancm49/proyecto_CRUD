a91831734cab8cdc0e979dca0d20367374c3bc70
Cristian Cortes Mondragon <ccortesm2000@gmail.com>
2022-05-11 20:36:27 -0500

    Se actualizo lista de clientes y registro de ellos

--- platzi_ventas/clients/commands.py
+++ platzi_ventas/clients/commands.py
@@ -1,5 +1,9 @@
+from http import client
+from unicodedata import name
 import click
 
+from clients.services import ClientService
+from clients.models import Client
 
 @click.group()
 def clients():
@@ -8,17 +12,49 @@ def clients():
 
 
 @clients.command()
+@click.option('-n', '--name',
+             type=str,
+             prompt=True,
+             help='El nombre del cliente')
+@click.option('-c', '--company',
+             type=str,
+             prompt=True,
+             help='La compania del cliente')
+@click.option('-e', '--email',
+             type=str,
+             prompt=True,
+             help='El email del cliente')
+@click.option('-p', '--position',
+             type=str,
+             prompt=True,
+             help='La position del cliente')
 @click.pass_context
 def create(ctx, name, company, email, position):
     """Create crea un nuevo cliente"""
-    pass
+    client = Client(name, company, email, position)
+    client_service = ClientService(ctx.obj['clients_table'])
+
+    client_service.create_client(client)
 
 
 @clients.command()
 @click.pass_context
 def list(ctx):
     """Lista de clientes"""
-    pass
+    client_service = ClientService(ctx.obj['clients_table'])
+
+    clients_list = client_service.list_clients()
+
+    click.echo('  ID  |  NAME  | COMANY  | EMAIL  | POSITION  ')
+    click.echo('*' * 100)
+
+    for client in clients_list:
+        print('{uid} | {name} | {company}| {email} | {position}'.format(
+            uid=client['uid'],
+            name=client['name'],
+            company=client['company'],
+            email=client['email'],
+            position=client['position'],))
 
 
 @clients.command()
